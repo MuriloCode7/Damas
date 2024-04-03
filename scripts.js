3152ce3daa607941b71d15392676207b542d3ab4
MuriloCode7 <murilomagalhaes60@gmail.com>
2024-04-03 20:32:14 -0300

    feat: finished pieces default insertion

--- scripts.js
+++ scripts.js
@@ -1,20 +1,55 @@
+const colorDarkCell = "black";
+const colorLightCell = "white";
+
 function drawBoard() {
   var board = document.getElementById("board");
 
-  for (let i = 0; i < 8; i++) {
-    var row = document.createElement("div");
-    row.id = `row:${i}`;
-    row.classList.add("row");
-    row.classList.add(i % 2 === 0 ? "even" : "odd");
-    board.appendChild(row);
+  for (let i = 1; i <= 8; i++) {
+    var col = document.createElement("div");
+    col.id = `col:${i}`;
+    col.classList.add("col", i % 2 === 0 ? "even" : "odd");
+    board.appendChild(col);
 
-    for (let j = 0; j < 8; j++) {
+    for (let j = 8; j >= 1; j--) {
       var cell = document.createElement("div");
-      cell.id = `cell:${i}.${j}`;
+      cell.id = `cell:${j}.${i}`;
       cell.classList.add("cell");
-      row.appendChild(cell);
+      col.appendChild(cell);
     }
   }
 }
 
+function isAllEven(num) {
+  let isEven = true;
+  num.forEach((n) => {
+    console.log(n % 2 === 0);
+    if (n % 2 !== 0) isEven = false;
+  });
+  return isEven;
+}
+
+function isAllOdd(num) {
+  let isOdd = true;
+  num.forEach((n) => {
+    if (n % 2 === 0) isOdd = false;
+  });
+  return isOdd;
+}
+
+function insertPieces() {
+  for (let i = 1; i <= 8; i++) {
+    for (let j = 1; j <= 8; j++) {
+      var currentCell = document.getElementById(`cell:${i}.${j}`);
+
+      if (isAllEven([i, j]) || isAllOdd([i, j])) {
+        var piece = document.createElement("div");
+        piece.id = i <= 3 ? "pieceLight" : "pieceDark";
+        currentCell.appendChild(piece);
+      }
+    }
+    if (i === 3) i = 5;
+  }
+}
+
 drawBoard();
+insertPieces();
